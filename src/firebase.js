11eea351caad38d6c59154d290a591eb986f69a1
fahmida76 <63741081+fahmida76@users.noreply.github.com>
2023-03-16 16:48:36 +0600

    upload video and update navbar

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCzqQAgiUh2d3zPLSAQOjxrHljGuclIpH4",
+  authDomain: "aqous-client.firebaseapp.com",
+  projectId: "aqous-client",
+  storageBucket: "aqous-client.appspot.com",
+  messagingSenderId: "696902821721",
+  appId: "1:696902821721:web:19ec6e585cad0dea358e48"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
